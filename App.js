8ea22dc074b016d7a05a609381bbdbbf24845701
Neo Joram <neojoram12@gmail.com>
2023-07-18 18:55:17 +0200

    Installations

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementor = useCallback(() => {
+    setCounter(counter + 1);
+  }, [setCounter, counter]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        placeholder="Welcome"
+        style={{
+          border: "1px solid black",
+          paddingHorizontal: "10px",
+          paddingVertical: "5px",
+          borderRadius: "12px",
+        }}
+      />
+      <select>
+        <option>One option</option>
+      </select>
+      <h3>Heading three</h3>
+      <span>This is span</span>
+      <table style={{ border: "1px solid black" }}>
+        <thead>
+          <tr>
+            <th>Table head</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Table data</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>Counter {counter}</h2>
+      <button onClick={() => incrementor()}>AddCounter</button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
